2312b9d98f412b94f3c4c1900dd05247f4a328c6
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2024-09-07 02:06:50 +0500

    commit

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -9,7 +9,7 @@ export default function CardCmp() {
   const [product, setProduct] = useState();
   const [loading, setloading] = useState(false);
   const [category, setCategory] = useState("all");
-  const [chosen , setChosen] = useState()
+  const [chosen, setChosen] = useState();
 
   useEffect(() => {
     const URL =
@@ -32,52 +32,49 @@ export default function CardCmp() {
   const setCat = (chip) => {
     // console.log("click", chip);
     setCategory(chip);
-    setChosen(chip)
+    setChosen(chip);
   };
-
-  console.log(category);
-  console.log(chosen);
 
   return (
     <>
-      <CategoryChips onClick={setCat} />
-      <section className="flex justify-center items-center">
-        <div className="gap-4 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 flex-wrap justify-center mx-7">
-          {loading ? (
-            <Spinner />
-          ) : (
-            product?.map((item, index) => (
-              <Link
-                key={index}
-                to={`/product/${item.id}`}
-                className="text-white"
-              >
-                <Card
-                  shadow="sm"
-                  isPressable
-                  onPress={() => console.log("item pressed")}
-                  className="p-0 w-full h-full bg-slate-800 text-white"
+        <CategoryChips onClick={setCat} category={category} />
+        <section className="flex justify-center items-center mt-14">
+          <div className="gap-4 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 flex-wrap justify-center mx-7">
+            {loading ? (
+              <Spinner />
+            ) : (
+              product?.map((item, index) => (
+                <Link
+                  key={index}
+                  to={`/product/${item.id}`}
+                  className="text-white"
                 >
-                  <CardBody className="overflow-visible p-0">
-                    <Image
-                      shadow="sm"
-                      radius="lg"
-                      width="100%"
-                      alt={item.title}
-                      className="w-full object-contain h-[140px]"
-                      src={item.thumbnail}
-                    />
-                  </CardBody>
-                  <CardFooter className="text-small justify-between">
-                    <b className="">{item.title}</b>
-                    <p className="text-default-500">${item.price}</p>
-                  </CardFooter>
-                </Card>
-              </Link>
-            ))
-          )}
-        </div>
-      </section>
+                  <Card
+                    shadow="sm"
+                    isPressable
+                    onPress={() => console.log("item pressed")}
+                    className="p-0 w-full h-full bg-slate-800 text-white"
+                  >
+                    <CardBody className="overflow-visible p-0">
+                      <Image
+                        shadow="sm"
+                        radius="lg"
+                        width="100%"
+                        alt={item.title}
+                        className="w-full object-contain h-[140px]"
+                        src={item.thumbnail}
+                      />
+                    </CardBody>
+                    <CardFooter className="text-small justify-between">
+                      <b className="">{item.title}</b>
+                      <p className="text-default-500">${item.price}</p>
+                    </CardFooter>
+                  </Card>
+                </Link>
+              ))
+            )}
+          </div>
+        </section>
     </>
   );
 }
--- src/Component/CategoryChips.jsx
+++ src/Component/CategoryChips.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const CategoryChips = ({ onClick }) => {
+const CategoryChips = ({ onClick, category }) => {
   const [categories, setCategories] = useState();
 
   useEffect(() => {
@@ -12,14 +12,16 @@ const CategoryChips = ({ onClick }) => {
       .catch((err) => console.log(err));
   }, []);
 
-  //   console.log(categories);
+  // categories.unshift("all")
 
   return (
     <>
       <section className="flex flex-wrap gap-4 p-3 justify-start">
         <div
           onClick={() => onClick("all")}
-          className="border border-gray-700 py-3 px-6 rounded-xl cursor-pointer hover:bg-slate-700 hover:text-white"
+          className={`${
+            category == "all" ? "bg-gray-900" : "border border-gray-700"
+          } py-3 px-6 rounded-xl cursor-pointer hover:bg-slate-700 hover:text-white`}
         >
           <p>All</p>
         </div>
@@ -27,7 +29,9 @@ const CategoryChips = ({ onClick }) => {
           <section key={index}>
             <div
               onClick={() => onClick(chip)}
-              className="border border-gray-700 py-3 px-6 rounded-xl cursor-pointer hover:bg-slate-700 hover:text-white"
+              className={`${
+                category == chip ? "bg-gray-900" : "border border-gray-700"
+              } py-3 px-6 rounded-xl cursor-pointer hover:bg-slate-700 hover:text-white`}
             >
               <p>{chip}</p>
             </div>
